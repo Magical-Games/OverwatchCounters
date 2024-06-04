c0a5773ab1a7e0f52322aace42c59338b87112c5
Offic <Offic@PC-MILAN>
2024-06-04 23:41:55 +0200

    Add project files.

---
+++ OverwatchCounters/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace OverwatchCounters
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "D.VA", "DOOMFIST", "JUNKER QUEEN", "MAUGA", "ORISA", "RAMATTRA", "REINHARDT", "ROADHOG", "SIGMA", "WINSTON", "WRECKING BALL", "ZARYA", "ASHE", "BASTION", "CASSIDY", "ECHO", "GENJI", "HANZO", "JUNKRAT", "MEI", "PHARAH", "REAPER", "SOJOURN", "SOLDIER: 67", "SOMBRA", "SYMMETRA", "TORBJORN", "TRACER", "VENTURE", "WIDOWMAKER", "ANA", "BAPISTE", "BRIGITTE", "ILLARY", "KIRIKO", "LIFEWEAVER", "LUCIO", "MERCY", "MOIRA", "ZENYATTA" });
+            comboBox1.Location = new Point(357, 36);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(254, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 1;
+            label1.Text = "You're Hero";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(254, 217);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Selected: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(317, 217);
+            label3.Name = "label3";
+            label3.Size = new Size(12, 15);
+            label3.TabIndex = 3;
+            label3.Text = "?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Overwatch Counters";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
