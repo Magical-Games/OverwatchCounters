a127a25530bec798daaec0c71a21428fd07ba7cf
Official Haures <OfficialFox2005@outlook.com>
2024-06-05 16:16:41 +0200

    Added some arrays for the start

--- OverwatchCounters/Form1.Designer.cs
+++ OverwatchCounters/Form1.Designer.cs
@@ -32,14 +32,16 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            listView1 = new ListView();
+            label4 = new Label();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "D.VA", "DOOMFIST", "JUNKER QUEEN", "MAUGA", "ORISA", "RAMATTRA", "REINHARDT", "ROADHOG", "SIGMA", "WINSTON", "WRECKING BALL", "ZARYA", "ASHE", "BASTION", "CASSIDY", "ECHO", "GENJI", "HANZO", "JUNKRAT", "MEI", "PHARAH", "REAPER", "SOJOURN", "SOLDIER: 67", "SOMBRA", "SYMMETRA", "TORBJORN", "TRACER", "VENTURE", "WIDOWMAKER", "ANA", "BAPISTE", "BRIGITTE", "ILLARY", "KIRIKO", "LIFEWEAVER", "LUCIO", "MERCY", "MOIRA", "ZENYATTA" });
-            comboBox1.Location = new Point(357, 36);
+            comboBox1.Items.AddRange(new object[] { "DVA", "DOOMFIST", "JUNKER QUEEN", "MAUGA", "ORISA", "RAMATTRA", "REINHARDT", "ROADHOG", "SIGMA", "WINSTON", "WRECKING BALL", "ZARYA", "ASHE", "BASTION", "CASSIDY", "ECHO", "GENJI", "HANZO", "JUNKRAT", "MEI", "PHARAH", "REAPER", "SOJOURN", "SOLDIER: 67", "SOMBRA", "SYMMETRA", "TORBJORN", "TRACER", "VENTURE", "WIDOWMAKER", "ANA", "BAPISTE", "BRIGITTE", "ILLARY", "KIRIKO", "LIFEWEAVER", "LUCIO", "MERCY", "MOIRA", "ZENYATTA" });
+            comboBox1.Location = new Point(287, 217);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(121, 23);
             comboBox1.TabIndex = 0;
@@ -49,7 +51,7 @@
             // 
             label1.AutoSize = true;
             label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(254, 39);
+            label1.Location = new Point(184, 220);
             label1.Name = "label1";
             label1.Size = new Size(69, 15);
             label1.TabIndex = 1;
@@ -59,7 +61,7 @@
             // 
             label2.AutoSize = true;
             label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(254, 217);
+            label2.Location = new Point(184, 288);
             label2.Name = "label2";
             label2.Size = new Size(57, 15);
             label2.TabIndex = 2;
@@ -69,18 +71,40 @@
             // 
             label3.AutoSize = true;
             label3.ForeColor = SystemColors.ButtonHighlight;
-            label3.Location = new Point(317, 217);
+            label3.Location = new Point(247, 288);
             label3.Name = "label3";
             label3.Size = new Size(12, 15);
             label3.TabIndex = 3;
             label3.Text = "?";
+            // 
+            // listView1
+            // 
+            listView1.BackColor = SystemColors.Highlight;
+            listView1.Location = new Point(497, 217);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(331, 281);
+            listView1.TabIndex = 4;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(471, 187);
+            label4.Name = "label4";
+            label4.Size = new Size(446, 15);
+            label4.TabIndex = 5;
+            label4.Text = "IMPORTANT: AFTER 10 SECONDS THE COUNTERS WILL DELETED IN THE MENU.";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaptionText;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1112, 635);
+            Controls.Add(label4);
+            Controls.Add(listView1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -97,5 +121,7 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private ListView listView1;
+        private Label label4;
     }
 }
--- OverwatchCounters/Form1.cs
+++ OverwatchCounters/Form1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace OverwatchCounters
 {
     public partial class Form1 : Form
@@ -7,9 +9,28 @@ namespace OverwatchCounters
             InitializeComponent();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+            string[] DVA = { "Zarya", "Doomfist", "Junker Queen", "Roadhog", "Sigma", "Reinhardt", "Mei", "Reaper", "Sombra", "Symmetra", "Moira", "Brigitte" };
+            string[] Doomfist = { "Roadhog", "Orisa", "Sigma", "Bastion", "Symettra", "Sombra", "Mei", "Cassidy", "Junkrat", "Anna", "Moira", "Zenyatta" };
+            string[] JunkerQueen = { "Mei", "Kiriko", "Ana", "Widowmaker", "Hanzo", "Ashe" };
+            string[] Mauga = { "Reaper", "DVA", "Ana", "Zenyatta", "Soldier: 76", "Sigma", "Sombra" };
+            string[] Orisa = { "Reaper", "Soldier: 76", "Zenyatta", "DVA", "Winston", "Zarya", "Sombra", "Symmetra", "Mei", "Zenyatta" };
+            string[] Ramattra = { "Sigma", "Orisa", "DVA", "Roadhog", "Sojourn", "Soldier: 76", "Reaper", "Bastion", "Tracer", "Junkrat", "Ana", "Baptiste", "Zenyatta", "Moira" };
+            string[] Reinhardt = { "Ramattra", "Orisa", "DVA", "Mauga", "Winston", "Bastion", "Sombra", "Soldier: 76", "Sojourn", "Tracer", "Symmetra", "Reaper", "Cassidy", "Junkrat", "Mei", "Brigitte", "Ana" };
+            string[] Roadhog = {"Zenyatta", "Ana", "Sombra", "Soldier: 76", "Sojourn" };
             label3.Text = comboBox1.Text;
+            if (comboBox1.Text == "DVA")
+            {
+                foreach (string i in DVA)
+                {
+                    listView1.Items.Add(i);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                listView1.Items.Clear();
+
+            }
         }
     }
 }
